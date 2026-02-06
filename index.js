3ed5e3abbf1a5872f4aa2b62add1b3275a48d1a9
Skurt246 <139325128+Skurt246@users.noreply.github.com>
2026-02-06 23:01:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const io = require('socket.io')(process.env.PORT || 3000, { cors: { origin: "*" } });
+const io = require('socket.io')(process.env.PORT || 3000, {
+    cors: { origin: "*" },
+    proxy: true,
+    transports: ['websocket'] // Только быстрые вебсокеты
+});
+
 let users = {};
 
-io.on('connection', (s) => {
-    s.on('join', (id) => { users[s.id] = id; io.emit('updateList', Object.values(users)); });
-    s.on('sync', (d) => s.broadcast.emit('action', d)); // Мгновенная пересылка действий
-    s.on('disconnect', () => { delete users[s.id]; io.emit('updateList', Object.values(users)); });
+io.on('connection', (socket) => {
+    // Регистрация в вайтлисте
+    socket.on('join', (id) => {
+        users[socket.id] = id;
+        io.emit('updateList', Object.values(users));
+    });
+
+    // ПРЕМИАЛЬНАЯ СИНХРОНИЗАЦИЯ (Volatile)
+    // volatile означает: если интернет мигнул, сервер выкинет старый пакет 
+    // и даст самый новый. Это убирает эффект "телепортации" назад.
+    socket.on('s', (data) => {
+        socket.broadcast.volatile.emit('a', data);
+    });
+
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.emit('updateList', Object.values(users));
+    });
 });
+
+console.log("Premium Server Started");
