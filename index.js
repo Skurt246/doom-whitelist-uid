e48764f31ff24ef421591a437e85766f4c9ecc62
Skurt246 <139325128+Skurt246@users.noreply.github.com>
2026-02-06 23:07:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const io = require('socket.io')(process.env.PORT || 3000, {
-    cors: { origin: "*" },
-    transports: ['websocket']
-});
-
+const io = require('socket.io')(process.env.PORT || 3000, { cors: { origin: "*" } });
 io.on('connection', (s) => {
-    // Получаем сжатый пакет и шлем остальным без задержек
     s.on('s', (d) => s.broadcast.volatile.emit('a', d));
 });
