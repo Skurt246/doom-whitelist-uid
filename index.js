a160148df225ac8db48f3c2b166536eb1019f90a
Skurt246 <139325128+Skurt246@users.noreply.github.com>
2026-02-06 23:02:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const io = require('socket.io')(process.env.PORT || 3000, {
     cors: { origin: "*" },
-    proxy: true,
-    transports: ['websocket'] // Только быстрые вебсокеты
+    transports: ['websocket']
 });
 
 let users = {};
 
 io.on('connection', (socket) => {
-    // Регистрация в вайтлисте
     socket.on('join', (id) => {
         users[socket.id] = id;
         io.emit('updateList', Object.values(users));
     });
 
-    // ПРЕМИАЛЬНАЯ СИНХРОНИЗАЦИЯ (Volatile)
-    // volatile означает: если интернет мигнул, сервер выкинет старый пакет 
-    // и даст самый новый. Это убирает эффект "телепортации" назад.
+    // Быстрая пересылка: Мастер -> Сервер -> Боты
     socket.on('s', (data) => {
         socket.broadcast.volatile.emit('a', data);
     });
@@ -25,5 +21,3 @@ io.on('connection', (socket) => {
         io.emit('updateList', Object.values(users));
     });
 });
-
-console.log("Premium Server Started");
