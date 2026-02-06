8c4d9cb80c6ffb9452121d9ccc7af7f00c29c130
Skurt246 <139325128+Skurt246@users.noreply.github.com>
2026-02-06 23:08:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const io = require('socket.io')(process.env.PORT || 3000, { cors: { origin: "*" } });
+const io = require('socket.io')(process.env.PORT || 3000, {
+    cors: { origin: "*", methods: ["GET", "POST"] },
+    transports: ['websocket'], // ОТКЛЮЧАЕМ polling
+    allowEIO3: true
+});
+
 io.on('connection', (s) => {
+    console.log('Connected:', s.id);
     s.on('s', (d) => s.broadcast.volatile.emit('a', d));
+    s.on('disconnect', () => console.log('Disconnected'));
 });
